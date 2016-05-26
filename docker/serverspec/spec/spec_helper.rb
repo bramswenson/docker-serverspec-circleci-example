e37451e963441c9e598cf18b50aa0361c4a1c9db
Bram Swenson <bram@craniumisajar.com>
2016-05-26 13:47:14 -0400

    Try the docker run hack instead of being stuck at old versions

--- docker/serverspec/spec/spec_helper.rb
+++ docker/serverspec/spec/spec_helper.rb
@@ -6,29 +6,43 @@ set :backend, :docker
 set :os, family: 'ubuntu', arch: 'x86_64'
 if ENV['DOCKER_IMAGE']
   set :docker_image, ENV['DOCKER_IMAGE']
-elsif ENV['DOCKER_CONTAINER']
+else
+  image = ::Docker::Image.build_from('../..')
+  ENV['DOCKER_IMAGE'] = image.id
+  set :docker_image, image.id
+end
+if ENV['DOCKER_CONTAINER']
   set :docker_container, ENV['DOCKER_CONTAINER']
 end
 
 # TODO https://github.com/swipely/docker-api/issues/202
-Excon.defaults[:ssl_verify_peer] = false
+#Excon.defaults[:ssl_verify_peer] = false
 
 # https://circleci.com/docs/docker#docker-exec
 if ENV['CIRCLECI']
   module Docker
     class Container
       def exec(command, opts = {}, &block)
+        #command[2] = command[2].inspect
+        #cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
+        #stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
+        #[stdout.read, [stderr.read], wait_thread.value.exitstatus]
         command[2] = command[2].inspect
-        cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
+        cmd = command.join(' ').gsub(/\$/, '\$')
+        if command[0] == '/bin/sh'
+          cmd = "docker run #{ENV['DOCKER_IMAGE']} #{cmd}"
+        else
+          cmd = "docker run --entrypoint=/bin/sh #{ENV['DOCKER_IMAGE']} -c #{cmd}"
+        end
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
 
-      def remove(options={})
-        # do not delete container
-      end
-      alias_method :delete, :remove
-      alias_method :kill, :remove
+      #def remove(options={})
+      #  # do not delete container
+      #end
+      #alias_method :delete, :remove
+      #alias_method :kill, :remove
     end
   end
 end
